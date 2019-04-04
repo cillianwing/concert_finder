2bf8fbd63739df3bc781e99c9662ae1987df215a
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-04 06:13:51 -0500

    playing around with scraper.rb to attempt getting webpage to load

--- lib/concert_finder/scraper.rb
+++ lib/concert_finder/scraper.rb
@@ -1,14 +1,16 @@
 class ConcertFinder::Scraper
 
   def get_main_page
-    doc = Nokogiri::HTML(open("https://www.songkick.com/metro_areas/9179-us-austin"))
+    Nokogiri::HTML(open("https://www.songkick.com/metro_areas/9179-us-austin"))
+    binding.pry
     # this page will likely have multiple pages. Need to be able to look at each. Mechanize?
   end
 
   def concert_dates
     # date = doc.css(".event-listings .with-date time").attribute("datetime").value
+    # artist = doc.css(".event-listings .artists summary strong").text <- not sure
     self.get_main_page.css(".event-listings").each do |x|
-      binding.pry
+      #binding.pry
     end
   end
 
